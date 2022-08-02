df5df691b6dd2fbeac78af305159027016886d74
Broad Vilks <27874709+broadvilks@users.noreply.github.com>
2022-08-02 16:44:22 -0500

    🗑 Remove hardhat/console

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,8 +1,6 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.3;
 
-import "hardhat/console.sol";
-
 contract Token {
   string public name = "Jared Borders Token";
   string public symbol = "JBT";
